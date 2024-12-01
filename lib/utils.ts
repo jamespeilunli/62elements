337e65655c1f1438d9821b19667f1b365d89f251
jamesli <jamespeilunli@gmail.com>
2024-12-01 14:31:55 -0800

    add fetching utility to utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const fetchFilteredData = async (url: string, setId: number, filterKey: string) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  // Filter data based on setId
+  return data.filter((item) => item[filterKey] === setId);
+};
