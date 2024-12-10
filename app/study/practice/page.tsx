8c4607bef53c7b7fc8d397030e8edbeb6903c995
James Li <jamespeilunli@gmail.com>
2024-12-09 21:48:28 -0800

    refactor new question setup code

--- app/study/practice/page.tsx
+++ app/study/practice/page.tsx
@@ -40,28 +40,29 @@ function PracticePage() {
     return answerType === "short-answer" || (answerType === "both" && Math.random() < 0.5);
   }, [answerType]);
 
-  const shuffleCards = useCallback(() => {
-    setShuffledCards([...flashcards].sort(() => Math.random() - 0.5));
-    setCurrentCardIndex(0);
-    setShowAnswer(false);
-    setIsTermQuestion(newIsTermQuestion());
-    setIsShortAnswerQuestion(newIsShortAnswerQuestion());
-  }, [newIsTermQuestion, newIsShortAnswerQuestion, flashcards]);
-
-  const nextQuestion = useCallback(() => {
+  const prepareQuestion = useCallback(() => {
     setAnswerSubmitted(false);
     setIsCorrect(false);
     setUserAnswer("");
     setShowAnswer(false);
     setIsTermQuestion(newIsTermQuestion());
     setIsShortAnswerQuestion(newIsShortAnswerQuestion());
+  }, [newIsTermQuestion, newIsShortAnswerQuestion]);
 
+  const shuffleCards = useCallback(() => {
+    setShuffledCards([...flashcards].sort(() => Math.random() - 0.5));
+    setCurrentCardIndex(0);
+    prepareQuestion();
+  }, [flashcards, prepareQuestion]);
+
+  const nextQuestion = useCallback(() => {
     if (currentCardIndex < shuffledCards.length - 1) {
       setCurrentCardIndex(currentCardIndex + 1);
+      prepareQuestion();
     } else {
       shuffleCards();
     }
-  }, [currentCardIndex, shuffledCards, shuffleCards]);
+  }, [currentCardIndex, shuffledCards, shuffleCards, prepareQuestion]);
 
   const validateAnswer = (guess: string, rightAnswer: string): boolean => {
     // normalize NFKD is used here to turn subscript and superscript characters to their normal counterparts
