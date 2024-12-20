2f754119d44db458caaa6ae67a6a97df6ab19106
jamesli <jamespeilunli@gmail.com>
2024-12-20 09:29:44 -0800

    fix some config files after running npx shadcn@latest init

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const fetchFilteredTable = async (url: string, setId: number, filterKey: string) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const filteredData = [];
+  for (const row of data) {
+    if (row[filterKey] == setId) {
+      filteredData.push(row);
+    }
+  }
+  return filteredData;
+};
